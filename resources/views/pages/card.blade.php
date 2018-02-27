ff0897e148ac2b1f444bfdda3c8f0015b83d744e
siddharth <sj564@njit.edu>
2018-02-27 17:32:59 -0500

    modification of card page

--- resources/views/pages/card.blade.php
+++ resources/views/pages/card.blade.php
@@ -1,8 +1,8 @@
 <div class="card">
     <div class="card-block">
-        <h4 class="card-title">Subject:</h4>
-        <h4 class="card-title">From Email:</h4>
-        <h4 class="card-title">Message:</h4>
+        <h4 class="card-title">Subject: XXXXX</h4>
+        <h4 class="card-title">From Email: XXXXX</h4>
+        <h4 class="card-title">Message: XXXXX</h4>
 
     </div>
 </div>
