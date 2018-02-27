26cb683ec3480f92b3a25ef889edf038d322c8e8
siddharth <sj564@njit.edu>
2018-02-26 20:26:30 -0500

    Changes in about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -6,7 +6,7 @@
 
 
         <h1>Hello everyone! This is the about page.</h1>
-        <p class="lead">This is the about page for the mini-project for IS601.<br> Kindly go through all other pages.</p>
+        <p class="lead">This is the about page for the mini-project for IS601.<br>Let's talk about game theory. <br>ndividual decision problems with stochastic outcomes are sometimes considered "one-player games". These situations are not considered game theoretical by some authors.[by whom?] They may be modeled using similar tools within the related disciplines of decision theory, operations research, and areas of artificial intelligence, particularly AI planning (with uncertainty) and multi-agent system. Although these fields may have different motivators, the mathematics involved are substantially the same, e.g. using Markov decision processes (MDP)</p>
 
 
         </div>
