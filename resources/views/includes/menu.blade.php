b718bb3a801f4943bef521a1e20b38ded40c5a68
siddharth <sj564@njit.edu>
2018-02-27 17:36:32 -0500

    Modified 'View Message' in menu to 'View Messages'

--- resources/views/includes/menu.blade.php
+++ resources/views/includes/menu.blade.php
@@ -16,7 +16,7 @@
                 <a class="nav-link" href="/contact">Contact</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/viewmessage">View Message</a>
+                <a class="nav-link" href="/viewmessage">View Messages</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="/signup">Sign Up</a>
